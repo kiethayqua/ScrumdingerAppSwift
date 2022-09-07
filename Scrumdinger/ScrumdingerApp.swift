8463b5be319fab8b1a3c80a8198a877f686fe656
kietnguyen99 <kiet.nguyen2@mservice.com.vn>
2022-09-07 09:47:27 +0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Nguyen Phuc Kiet on 07/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
